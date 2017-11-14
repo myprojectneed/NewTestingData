fb5afd21579198e72c90aae5c9084e25e73ef497
myprojectneed <33648000+myprojectneed@users.noreply.github.com>
2017-11-14 10:53:01 +0530

    Code committed for Java

---
+++ MYHelloJava.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class MYHelloJava {
+	public static void main (String[] args){
+
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		
+		//System.out.printIn("Hello Java");
+		
+		
+	}
+
+}
